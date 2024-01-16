8fbdd5e4b9e4f7ffbda6e7f9323b5d27668f14a8
Erta T <93384661+sh33py01@users.noreply.github.com>
2024-01-16 16:46:33 +0100

    Delete Netflix_Merge_Data.py

--- Netflix_Merge_Data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_IMDb_movies = pd.read_csv('movies.csv')
-df_IMDb_names = pd.read_csv('Netflix_User_Ratings.csv')
-
-
-df_combined = pd.merge(df_IMDb_movies, df_IMDb_names, on='MovieId')
-df_combined.to_csv('Netflix_movies_archive2.csv', index=False)
