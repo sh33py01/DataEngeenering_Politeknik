730c210e75cf5be66adb41090344bb31159baae0
Erta T <93384661+sh33py01@users.noreply.github.com>
2024-01-16 16:46:46 +0100

    Delete IMDB_csv_merge.py

--- IMDB_csv_merge.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_IMDb_movies = pd.read_csv('IMDb movies.csv')
-df_IMDb_names = pd.read_csv('IMDb names.csv')
-
-
-df_combined = pd.merge(df_IMDb_movies, df_IMDb_names, on='imdb_title_id')
-df_combined.to_csv('IMDB_movies_archive1.csv', index=False)
